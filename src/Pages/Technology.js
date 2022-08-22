44c2c5c02fb207250255e077df43de4fdb26c3f4
Nathon Reed <nrwebdev00@gmail.com>
2022-08-22 07:24:13 -0700

    fisnhed desktop tech

--- src/Pages/Technology.js
+++ src/Pages/Technology.js
@@ -6,9 +6,16 @@ import backgroundDesktop from '../Img/technology/background-technology-desktop.j
 import backgroundTablet from '../Img/technology/background-technology-tablet.jpg';
 import backgroundMobile from '../Img/technology/background-technology-mobile.jpg';
 
+import data from '../Data/data';
+
+import spaceport from '../Img/technology/image-spaceport-portrait.jpg';
+import capsule from '../Img/technology/image-space-capsule-portrait.jpg';
+import launch from '../Img/technology/image-launch-vehicle-portrait.jpg';
+
 export default function Technology() {
 
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [tech, setTech] = useState('Launch')
 
   useEffect(() =>{
     const handleWindowResize = () =>{
@@ -21,6 +28,41 @@ export default function Technology() {
     }
   }, []);
 
+  const techImage = () =>{
+    if(tech === 'Launch'){
+      return launch
+    }
+    if(tech === 'Capsule'){
+      return capsule
+    }
+    if(tech === 'Spaceport'){
+      return spaceport
+    }
+  }
+
+  const techTitle = () =>{
+    if(tech === 'Launch'){
+      return data.technology[0].name;
+    }
+    if(tech === 'Spaceport'){
+      return data.technology[1].name;
+    }
+    if(tech === 'Capsule'){
+      return data.technology[2].name;
+    }
+  }
+
+  const techText = () =>{
+    if(tech === 'Launch'){
+      return data.technology[0].description;
+    }
+    if(tech === 'Spaceport'){
+      return data.technology[1].description;
+    }
+    if(tech === 'Capsule'){
+      return data.technology[2].description
+    }
+  }
 
   const responsiveBackgroundImage = () =>{
     if(windowWidth > 1028){
@@ -41,6 +83,47 @@ export default function Technology() {
       style={{ backgroundImage: `url(${responsiveBackgroundImage()})`}}
     >
       <Header active='Technology' />
+      <main className="tech">
+        <div className="tech__title subheading-1"><span className="darken-text padding-right-4">03  </span> Space Launch 101</div>
+        <div className="wrapper padding-top-6">
+          <section className="left padding-top-10">
+            <div className="left-inner">
+              <div
+                className={`number heading-5 ${tech === 'Launch' ? 'number-active':''}`}
+                onClick={(() => setTech('Launch'))}
+                >
+                  1
+                </div>
+              <div
+                className={`number heading-5 ${tech === 'Spaceport' ? 'number-active' : ''}`}
+                onClick={(() => setTech('Spaceport'))}
+                >
+                  2
+                </div>
+              <div
+                className={`number heading-5 ${tech === 'Capsule' ? 'number-active' : ''}`}
+                onClick={(() => setTech('Capsule'))}
+                >
+                  3
+                </div>
+            </div>
+            <div className="right-inner">
+              <div className="right-inner__title heading-5 padding-bottom-4">
+                The Terminology...
+              </div>
+              <div className="right-inner__name heading-3 padding-bottom-4">
+                {techTitle()}
+              </div>
+              <div className="right-inner__text body-text">
+                {techText()}
+              </div>
+            </div>
+          </section>
+          <section className="right">
+            <img src={techImage()} alt="" />
+          </section>
+        </div>
+      </main>
     </div>
   )
 }
